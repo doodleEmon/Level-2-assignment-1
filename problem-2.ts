9640f8e6a439e73bde22e49831d86bd3f8b45ad2
Emon <150001421+doodleEmon@users.noreply.github.com>
2024-11-24 13:21:49 +0600

    Update problem-2.ts

--- problem-2.ts
+++ problem-2.ts
@@ -1,15 +1,5 @@
-const findLargestNumber = (arr: number[]): number | undefined => {
-    if(arr.length === 0) {
-        return undefined;
-    }
-    
-    let largeNumber = arr[0];
-    for(let i = 1; i < arr.length; i++) {
-        if(arr[i] > largeNumber) {
-            largeNumber = arr[i];
-        }
-    }
-    return largeNumber;
+const largestElement = (arr: number[]): number | undefined => {
+    return arr.reduce((largest, current) => (current > largest ? current : largest), arr[0]);
 }
 
-findLargestNumber([10, 5, 8, 20, 3]);
+largestElement([10, 5, 8, 20, 3]);
